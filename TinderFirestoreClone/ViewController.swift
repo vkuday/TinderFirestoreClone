dc05a05d889fb8503ab39ae28fcb4d82834aff15
vkuday <volkuday@hotmail.com>
2022-03-10 16:34:18 -0800

    Initial Commit

---
+++ TinderFirestoreClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinderFirestoreClone
+//
+//  Created by yupana on 3/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
