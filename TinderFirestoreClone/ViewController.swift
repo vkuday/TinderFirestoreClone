3723c1db37fc4f3012f865ccf78d55c8045b1069
vkuday <volkuday@hotmail.com>
2022-03-10 17:47:12 -0800

    - Initialized the project. - Started creating mainpage

--- TinderFirestoreClone/ViewController.swift
+++ TinderFirestoreClone/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  TinderFirestoreClone
 //
-//  Created by yupana on 3/10/22.
+//  Created by Volkan Kuday on 3/10/22.
 //
 
 import UIKit
@@ -11,7 +11,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { color -> UIView in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subviews)
+        topStackView.distribution = .fillEqually
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .systemBlue
+        
+        let yellowView = UIView()
+        yellowView.backgroundColor = .systemYellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        
+        stackView.fillSuperview()
+        
     }
 
 
