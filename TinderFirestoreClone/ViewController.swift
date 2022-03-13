2167b813364b3732fc43dc979c9ff90e1caf4e5c
vkuday <volkuday@hotmail.com>
2022-03-12 23:57:50 -0800

    Implemented cardview

--- TinderFirestoreClone/ViewController.swift
+++ TinderFirestoreClone/ViewController.swift
@@ -10,24 +10,35 @@ import UIKit
 class ViewController: UIViewController {
     
     let topStackView = TopNavigationStackView()
-    let blueView = UIView()
+    let cardsDeckView = UIView()
     let buttonStackView = HomeBottomControlsStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        blueView.backgroundColor = .systemBlue
         setupLayout()
+        
+        setupDummyCards()
     }
     
     // MARK:- Fileprivate
     
+    fileprivate func setupDummyCards() {
+        let cardView = CardView()
+        cardsDeckView.addSubview(cardView)
+        cardView.fillSuperview()
+    }
+    
     fileprivate func setupLayout() {
-        let overallStackView = UIStackView(arrangedSubviews: [topStackView, blueView, buttonStackView])
+        let overallStackView = UIStackView(arrangedSubviews: [topStackView, cardsDeckView, buttonStackView])
         overallStackView.axis = .vertical
         view.addSubview(overallStackView)
         
         overallStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+        overallStackView.isLayoutMarginsRelativeArrangement = true
+        overallStackView.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
+        
+        overallStackView.bringSubviewToFront(cardsDeckView)
     }
 }
 
---
+++ TinderFirestoreClone/Views/CardView.swift
@@ -0,0 +1,52 @@
+//
+//  CardView.swift
+//  TinderFirestoreClone
+//
+//  Copyright © 2022 Volkan Kuday. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        addSubview(imageView)
+        imageView.fillSuperview()
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addGestureRecognizer(panGesture)
+    }
+    
+    @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .changed:
+            handleChanged(gesture)
+        case .ended:
+            handleEnded()
+        default:
+            break
+        }
+    }
+    
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: nil)
+        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+    }
+    
+    fileprivate func handleEnded() {
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut) {
+            self.transform = .identity
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
