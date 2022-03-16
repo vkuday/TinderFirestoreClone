b80c7ad5cb35905a3dd02d321ae75aac469749a8
vkuday <volkuday@hotmail.com>
2022-03-16 13:55:17 -0700

    Implemented Card ViewModel.

---
+++ TinderFirestoreClone/ViewModels/CardViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  CardViewModel.swift
+//  TinderFirestoreClone
+//
+//  Copyright © 2022 Volkan Kuday. All rights reserved.
+//
+
+import UIKit
+
+struct CardViewModel {
+    let imageName: String
+    let attributedString: NSAttributedString
+    let textAlignment: NSTextAlignment
+}
+
+
