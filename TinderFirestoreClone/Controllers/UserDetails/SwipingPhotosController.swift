dfd3af6456a5aa32c446930b2f72462a95333d8c
vkuday <volkuday@hotmail.com>
2022-05-11 11:51:18 -0700

    Implemented swiping photo bars

--- TinderFirestoreClone/Controllers/UserDetails/SwipingPhotosController.swift
+++ TinderFirestoreClone/Controllers/UserDetails/SwipingPhotosController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SwipingPhotosController: UIPageViewController, UIPageViewControllerDataSource {
+class SwipingPhotosController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
     
     var cardViewModel: CardViewModel! {
         didSet {
@@ -17,7 +17,27 @@ class SwipingPhotosController: UIPageViewController, UIPageViewControllerDataSou
             })
             
             setViewControllers([controllers.first!], direction: .forward, animated: false)
+            
+            setupBarViews()
         }
+    }
+    
+    fileprivate let barsStackView = UIStackView(arrangedSubviews: [])
+    fileprivate let deselectedBarColor = UIColor(white: 0, alpha: 0.1)
+    
+    fileprivate func setupBarViews() {
+        cardViewModel.imageUrls.forEach { _ in
+            let barView = UIView()
+            barView.backgroundColor = deselectedBarColor
+            barView.layer.cornerRadius = 2
+            barsStackView.addArrangedSubview(barView)
+        }
+        barsStackView.arrangedSubviews.first?.backgroundColor = .white
+        barsStackView.spacing = 4
+        barsStackView.distribution = .fillEqually
+        
+        view.addSubview(barsStackView)
+        barsStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 8, left: 8, bottom: 0, right: 8), size: .init(width: 0, height: 4))
     }
     
     var controllers = [UIViewController]()
@@ -25,7 +45,7 @@ class SwipingPhotosController: UIPageViewController, UIPageViewControllerDataSou
     override func viewDidLoad() {
         super.viewDidLoad()
         dataSource = self
-        
+        delegate = self
         view.backgroundColor = .white
     }
     
@@ -39,6 +59,14 @@ class SwipingPhotosController: UIPageViewController, UIPageViewControllerDataSou
         let index = self.controllers.firstIndex(where: { $0 == viewController }) ?? 0
         if index == controllers.count - 1 { return nil }
         return controllers[index + 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let currentPhotoController = viewControllers?.first
+        if let index = controllers.firstIndex(where: { $0 == currentPhotoController }) {
+            barsStackView.arrangedSubviews.forEach({ $0.backgroundColor = deselectedBarColor })
+            barsStackView.arrangedSubviews[index].backgroundColor = .white
+        }
     }
 }
 
